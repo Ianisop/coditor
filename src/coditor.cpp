31c86f268d0c814962150872c0da40a4385f812d
SomethingFishy <69714456+Ianisop@users.noreply.github.com>
2025-09-05 14:06:13 +0200

    fixed layout sizing, added a few buttons to toolbar, fixed dragging

--- src/coditor.cpp
+++ src/coditor.cpp
@@ -12,56 +12,72 @@ int display_w = 1280;
 int display_h = 720;
 std::string text_buffer = "Welcome to coditor";
 GLFWwindow* window;
-float file_explorer_width;
 
 bool dragging = false;
 ImVec2 drag_offset;
+double offsetX, offsetY;
+
+
+float toolbar_height     = 30.0f;
+float status_bar_height  = 30.0f;
+float file_explorer_width = 200.0f;
 
 void DrawToolbar()
 {
     ImGui::SetNextWindowPos(ImVec2(0, 0));
-    ImGui::SetNextWindowSize(ImVec2(display_w, 0));
+    ImGui::SetNextWindowSize(ImVec2(display_w, toolbar_height));
 
     ImGuiWindowFlags flags =
                              ImGuiWindowFlags_NoResize   |
                              ImGuiWindowFlags_NoMove     |
                              ImGuiWindowFlags_NoCollapse |
-                             ImGuiWindowFlags_NoScrollbar;
+                             ImGuiWindowFlags_NoScrollbar|
+                             ImGuiWindowFlags_NoTitleBar;
 
-    ImGui::Begin("Toolbar", nullptr, flags);
+    ImGui::Begin("Coditor", nullptr, flags);
 
     // Example buttons
-    if (ImGui::Button("New")) {}
+    ImGui::LabelText("Coditor","");
     ImGui::SameLine();
-    if (ImGui::Button("Open")) {}
+    if (ImGui::Button("-")) {}
     ImGui::SameLine();
-    if (ImGui::Button("Save")) {}
+    if (ImGui::Button("O")) {}
+    ImGui::SameLine();
+    if (ImGui::Button("x"))
+    {
+        glfwSetWindowShouldClose(window, true);
+    }
+
+
 
     if (ImGui::IsWindowHovered() && ImGui::IsMouseClicked(ImGuiMouseButton_Left)) {
         dragging = true;
 
-        // Store offset
         double mx, my;
-        glfwGetCursorPos(window, &mx, &my);
-        int wx, wy;
-        glfwGetWindowPos(window, &wx, &wy);
-
-        drag_offset = ImVec2((float)mx - (float)wx, (float)my - (float)wy);
+        glfwGetCursorPos(window, &mx, &my); // mouse inside window
+        offsetX = mx;
+        offsetY = my;
     }
 
     if (dragging && ImGui::IsMouseDown(ImGuiMouseButton_Left)) {
         double mx, my;
         glfwGetCursorPos(window, &mx, &my);
 
-        // Snap window position so mouse stays aligned
-        int new_x = (int)(mx - drag_offset.x);
-        int new_y = (int)(my - drag_offset.y);
-        glfwSetWindowPos(window, new_x, new_y);
+        // Get mouse in screen coords
+        int wx, wy;
+        glfwGetWindowPos(window, &wx, &wy);
+        int screenX = wx + (int)mx;
+        int screenY = wy + (int)my;
+
+        // Move window so the cursor stays at the same spot inside it
+        glfwSetWindowPos(window, screenX - (int)offsetX, screenY - (int)offsetY);
     }
 
-    if (dragging && ImGui::IsMouseReleased(ImGuiMouseButton_Left)) {
+    if (ImGui::IsMouseReleased(ImGuiMouseButton_Left)) {
         dragging = false;
     }
+
+
 
     ImGui::End();
 }
@@ -70,43 +86,26 @@ void DrawToolbar()
 
 void Run()
 {
-    // Menu bar
-    float menu_bar_height = 0.0f;
-    if (ImGui::BeginMainMenuBar()) {
-        menu_bar_height = ImGui::GetWindowSize().y;
-        if (ImGui::BeginMenu("File")) {
-            if (ImGui::MenuItem("Open")) { /* load file */ }
-            if (ImGui::MenuItem("Save")) { /* save file */ }
-            ImGui::EndMenu();
-        }
-        ImGui::EndMainMenuBar();
-    }
 
-    // Sizes
-    file_explorer_width = ImGui::GetWindowWidth();  // left panel width
-    float status_bar_height   = 20.0f;   // bottom bar height
-    float editor_width       = display_w - file_explorer_width;
-    float editor_height      = display_h - menu_bar_height - status_bar_height;
+    float editor_width  = display_w - file_explorer_width;
+    float editor_height = display_h - toolbar_height - status_bar_height;
 
     DrawToolbar();
 
-    // File Tree
-    ImGui::SetNextWindowPos(ImVec2(0, menu_bar_height));
+    // --- File Explorer ---
+    ImGui::SetNextWindowPos(ImVec2(0, toolbar_height));
     ImGui::SetNextWindowSize(ImVec2(file_explorer_width, editor_height));
     ImGuiWindowFlags side_flags = ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove;
     ImGui::Begin("Files", nullptr, side_flags);
     ImGui::Text("File Explorer");
     ImGui::Separator();
-
-    //TODO: Add functionality to show and find files 
-    // Example mock files
-    if (ImGui::Selectable("main.cpp")) { /* load file */ }
-    if (ImGui::Selectable("coditor.h")) { /* load file */ }
-    if (ImGui::Selectable("CMakeLists.txt")) { /* load file */ }
+    if (ImGui::Selectable("main.cpp")) {}
+    if (ImGui::Selectable("coditor.h")) {}
+    if (ImGui::Selectable("CMakeLists.txt")) {}
     ImGui::End();
 
-    // Editor
-    ImGui::SetNextWindowPos(ImVec2(file_explorer_width, menu_bar_height));
+    // --- Editor ---
+    ImGui::SetNextWindowPos(ImVec2(file_explorer_width, toolbar_height));
     ImGui::SetNextWindowSize(ImVec2(editor_width, editor_height));
     ImGuiWindowFlags editor_flags = ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoCollapse;
     ImGui::Begin("Editor", nullptr, editor_flags);
@@ -114,17 +113,16 @@ void Run()
     ImGui::InputTextMultiline("##editor", &text_buffer[0], text_buffer.size() + 1024, content_size);
     ImGui::End();
 
-    // Status bar
+    // --- Status bar ---
     ImGui::SetNextWindowPos(ImVec2(0, display_h - status_bar_height));
     ImGui::SetNextWindowSize(ImVec2(display_w, status_bar_height));
     ImGuiWindowFlags status_flags = ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoResize |
                                     ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoCollapse;
     ImGui::Begin("StatusBar", nullptr, status_flags);
-
-    //TODO: make the status bar actually show metadata
     ImGui::Text("Ln 1, Col 1    |    UTF-8    |    Ready");
     ImGui::End();
 }
+
 
 
 int main() 
